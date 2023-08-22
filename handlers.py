37670ce35232b2649a84c8c701a8f52b6f408330
NikBel523 <belyakov.nkt@gmail.com>
2023-08-22 10:31:12 +0300

    feat: add handlers to use database functions with user input

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 from aiogram import types, Dispatcher
 
 from create_bot import dp, bot
-from data_base.sqlite_db import sql_add_film
+from data_base.sqlite_db import sql_add_film, sql_change_status, sql_show_suggestions, sql_delete_film
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -12,13 +12,44 @@ async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm soon became a FridayCinemaClubBot! Unfortunately, I'm not it now.")
 
 
-@dp.message_handler()
+@dp.message_handler(lambda message: "+" in message.text)
 async def add_film(message: types.Message):
-    film = (message.text,)
+    film = (message.text[2:], "active")
     await sql_add_film(film)
-    await message.answer(f"{message.text} added to db")
+    await message.answer(f"add {message.text[2:]} to db")
+
+
+@dp.message_handler(lambda message: "-" in message.text)
+async def check(message: types.Message):
+    film = (message.text[2:],)
+    await sql_change_status(film)
+    await message.answer(f"status {message.text} changed to watched")
+
+
+@dp.message_handler(lambda message: "watched" or "active" in message.text)
+async def show_films(message: types.Message):
+    if "watched" in message.text:
+        await sql_show_suggestions(message, ("watched",))
+    else:
+        await sql_show_suggestions(message, ("active",))
+
+
+@dp.message_handler(lambda message: "delete" in message.text)
+async def delete_films(message: types.Message):
+    film = (message.text[7:],)
+    await sql_delete_film(film)
+    await message.answer(f"Film {film} deleted")
+
+
+@dp.message_handler()
+async def show_info(message: types.Message):
+    await message.answer("There is no such command.")
+    await message.answer("Here is a list of active commands:")
 
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(send_welcome, commands=['start', 'help'])
     dp.register_message_handler(add_film)
+    dp.register_message_handler(check)
+    dp.register_message_handler(show_films)
+    dp.register_message_handler(show_info)
