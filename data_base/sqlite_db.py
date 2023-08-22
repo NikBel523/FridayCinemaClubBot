dce33eaa811632c10b7e27e1451723da6edddcc2
NikBel523 <belyakov.nkt@gmail.com>
2023-08-22 11:08:29 +0300

    docs: Add docstrings and comments to functions in handlers and database.

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -4,30 +4,79 @@ from create_bot import bot
 
 
 def sql_start():
+    """
+    Establishes a connection to the database and creates the films table if it doesn't exist.
+
+    This function initializes a connection to the SQLite database named "friday_films.db".
+    It creates a cursor for database operations and checks the connection status.
+    The films table is created if it doesn't already exist, with columns 'film' and 'status'.
+
+    :return: None
+    """
     global base, cur
     base = sq.connect("friday_films.db")
     cur = base.cursor()
     if base:
-        print("Data base connection ok")
+        print("Database connection established.")
     base.execute("CREATE TABLE IF NOT EXISTS films(film TEXT, status TEXT)")
     base.commit()
 
 
-async def sql_add_film(film):
+async def sql_add_film(film: tuple):
+    """
+    Adds a film to the films table in the database.
+
+    This asynchronous function inserts a new film entry into the films table
+    with the provided film name and status. It then commits the changes to the database.
+
+    :param film: A tuple containing the film name and its status.
+    :return: None
+    """
     cur.execute("INSERT INTO films VALUES (?, ?)", film)
     base.commit()
 
 
-async def sql_change_status(film):
+async def sql_change_status(film: tuple):
+    """
+    Changes the status of a film to 'watched' in the films table.
+
+    This asynchronous function updates the status of a film in the films table
+    to 'watched' based on the provided film name. It then commits the changes to the database.
+
+    :param film: A tuple containing the film name.
+    :type film: tuple
+    :return: None
+    """
     cur.execute("UPDATE films SET status = 'watched' WHERE film = (?)", film)
     base.commit()
 
 
 async def sql_show_suggestions(message, status):
+    """
+    Retrieves and sends film suggestions based on the provided status.
+
+    This asynchronous function queries the films table for films with the specified status.
+    It sends film suggestions to the user's ID using the provided message object.
+
+    :param message: The user's message triggering the function.
+    :type message: types.Message
+    :param status: The desired status of films to retrieve.
+    :type status: tuple
+    :return: None
+    """
     for film in cur.execute("SELECT film FROM films WHERE status = (?)", status):
         await bot.send_message(message.from_user.id, text=film[0])
 
 
-async def sql_delete_film(film):
+async def sql_delete_film(film: tuple):
+    """
+    Deletes a film from the films table in the database.
+
+    This asynchronous function removes a film entry from the films table based on the provided film name.
+    It then commits the changes to the database.
+
+    :param film: A tuple containing the film name.
+    :return: None
+    """
     cur.execute("DELETE FROM films WHERE film == ?", film)
     base.commit()
--- handlers.py
+++ handlers.py
@@ -14,20 +14,61 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(lambda message: "+" in message.text)
 async def add_film(message: types.Message):
-    film = (message.text[2:], "active")
+    """
+    Adds a film to the database with the default status set to 'active'.
+
+    This function handles messages that contain a '+' sign followed by a film name.
+    The provided film name is extracted from the message text, and the film is then added
+    to the database with its status set to 'active'.
+
+    :param message: The message from the user, which includes the '+' sign and the film name.
+    :return: None
+    """
+    # Extract the film name from the message text and set its status to 'active'
+    film_name = message.text[2:]
+    film = (film_name, "active")
+
+    # Add the film to the database
     await sql_add_film(film)
-    await message.answer(f"add {message.text[2:]} to db")
+
+    # Notify the user about the successful addition of the film
+    await message.answer(f"Added '{film_name}' to the database.")
 
 
 @dp.message_handler(lambda message: "-" in message.text)
 async def check(message: types.Message):
-    film = (message.text[2:],)
+    """
+    Changes the status of a film to "watched".
+
+    This function handles messages that contain a '-' sign followed by a film name.
+    The provided film name is extracted from the message text, and the status of the
+    corresponding film is changed to 'watched' in the database.
+
+    :param message: The user's message containing the '-' sign and the film name.
+    :return: None
+    """
+    # Extract the film name from the message text
+    film_name = message.text[2:]
+
+    # Change the status of the film to 'watched' in the database
+    film = (film_name,)
     await sql_change_status(film)
-    await message.answer(f"status {message.text} changed to watched")
+
+    # Notify the user about the successful status change
+    await message.answer(f"Status of '{film_name}' changed to 'watched'.")
 
 
 @dp.message_handler(lambda message: "watched" or "active" in message.text)
 async def show_films(message: types.Message):
+    """
+    Displays a list of films based on their status.
+
+    Depending on the presence of the keywords "watched" or "active" in the message text,
+    this function retrieves and displays a list of films that match the specified status.
+
+    :param message: The user's message containing keywords "watched" or "active".
+    :return: None
+    """
     if "watched" in message.text:
         await sql_show_suggestions(message, ("watched",))
     else:
@@ -36,14 +77,43 @@ async def show_films(message: types.Message):
 
 @dp.message_handler(lambda message: "delete" in message.text)
 async def delete_films(message: types.Message):
-    film = (message.text[7:],)
+    """
+    Deletes a film from the database.
+
+    This function handles messages that contain the keyword "delete" followed by a film name.
+    The provided film name is extracted from the message text, and the corresponding film is
+    deleted from the database.
+
+    :param message: The user's message containing the "delete" keyword and the film name.
+    :return: None
+    """
+    # Extract the film name from the message text
+    film_name = message.text[7:]
+
+    # Delete the film from the database
+    film = (film_name,)
     await sql_delete_film(film)
-    await message.answer(f"Film {film} deleted")
+
+    # Notify the user about the successful deletion
+    await message.answer(f"Film '{film_name}' deleted.")
 
 
 @dp.message_handler()
 async def show_info(message: types.Message):
+    """
+    Provides information about available commands.
+
+    This function is triggered when no specific command matches the user's message.
+    It responds by informing the user that there is no such command and then provides
+    a list of active commands that the user can use.
+
+    :param message: The user's message triggering the function.
+    :return: None
+    """
+    # Inform the user about the absence of a matching command
     await message.answer("There is no such command.")
+
+    # Provide a list of active commands
     await message.answer("Here is a list of active commands:")
 
 
