d20b3979fce1b1e4cda8f48244339c375f77af61
NikBel523 <belyakov.nkt@gmail.com>
2023-08-24 11:14:18 +0300

    feat: functions to add several filmes to a database and to simultaneously change status of active films.

--- handlers/manage_hand.py
+++ handlers/manage_hand.py
@@ -4,6 +4,31 @@ from create_bot import dp
 from data_base.sqlite_db import sql_add_film, sql_change_status, sql_delete_film
 
 
+@dp.message_handler(lambda message: "+++" in message.text)
+async def add_films(message: types.Message):
+    """
+    Adds several films to the database with the default status set to 'active'.
+
+    This function handles messages that contain a '+++' sign followed by a film name separate by a '\n'.
+    The provided films names are extracted from the message text, and films is then added
+    to the database with their status set to 'active'.
+
+    :param message: The message from the user, which includes the '+++' sign and several films names.
+    :return: None
+    """
+    # Extract the list films names from the message text
+    films_list = message.text.split("\n")
+    # Extract the films names from the message text and set its status to 'active'
+    for i in range(1, len(films_list)):
+        film = (films_list[i], "active")
+
+        # Add the film to the database
+        await sql_add_film(film)
+
+    # Notify the user about the successful addition of the film
+        await message.answer(f"Added '{films_list[i]}' to the database.")
+
+
 @dp.message_handler(lambda message: "+" in message.text)
 async def add_film(message: types.Message):
     """
@@ -27,6 +52,30 @@ async def add_film(message: types.Message):
     await message.answer(f"Added '{film_name}' to the database.")
 
 
+@dp.message_handler(lambda message: "---" in message.text or "—-" in message.text)
+async def check_films(message: types.Message):
+    """
+    Changes the status of several films to "watched".
+
+    This function handles messages that contain a '---' or '—-' signs
+    followed by several names of films separated by a '\n'.
+    The provided films names are extracted from the message text, and the status of the
+    corresponding films is changed to 'watched' in the database.
+
+    :param message: The user's message containing the '---' or '—-' signs and several names of films.
+    :return: None
+    """
+    # Extract all names from the message text to a list
+    films_list = message.text.split("\n")
+    # Change the status of each film to 'watched' in the database
+    for i in range(1, len(films_list)):
+        film = (films_list[i],)
+        await sql_change_status(film)
+
+        # Notify the user about the successful status change
+        await message.answer(f"Status of '{films_list[i]}' changed to 'watched'.")
+
+
 @dp.message_handler(lambda message: "-" in message.text)
 async def check(message: types.Message):
     """
@@ -41,7 +90,6 @@ async def check(message: types.Message):
     """
     # Extract the film name from the message text
     film_name = message.text[2:]
-
     # Change the status of the film to 'watched' in the database
     film = (film_name,)
     await sql_change_status(film)
@@ -74,6 +122,8 @@ async def delete_films(message: types.Message):
 
 
 def register_manage_handlers(dp: Dispatcher):
+    dp.register_message_handler(add_films)
     dp.register_message_handler(add_film)
+    dp.register_message_handler(check_films)
     dp.register_message_handler(check)
     dp.register_message_handler(delete_films)
