4ad2a590950e0fefffcc401432807c7787505306
NikBel523 <belyakov.nkt@gmail.com>
2023-08-31 13:14:10 +0300

    feat: add webhook option for a bot start up.

--- tg_bot.py
+++ tg_bot.py
@@ -1,7 +1,11 @@
+import os
+
 from aiogram.utils import executor
-from create_bot import dp
+from create_bot import dp, bot
 from data_base import sqlite_db
 from handlers import manage_hand, view_hand
+
+import config
 
 
 async def on_startup(_):
@@ -9,9 +13,32 @@ async def on_startup(_):
     sqlite_db.sql_start()
 
 
+async def on_startup_webhook(_):
+    print("Bot online")
+    sqlite_db.sql_start()
+    await bot.set_webhook(config.URL_APP)
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+
 manage_hand.register_manage_handlers(dp=dp)
 view_hand.register_view_handlers(dp=dp)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    if config.START_UP == "polling":
+        print(f"working with {config.START_UP}")
+        executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    else:
+        print(f"working with {config.START_UP}")
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path="",
+            on_startup=on_startup_webhook,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 5000))
+        )
