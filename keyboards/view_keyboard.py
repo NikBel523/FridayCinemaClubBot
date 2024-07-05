bb355060cb6abdb631882188bceb4727bb622656
NikBel523 <belyakov.nkt@gmail.com>
2024-07-05 17:02:46 +0300

    refactor: change keyboard to comply with aiogram 3

--- keyboards/view_keyboard.py
+++ keyboards/view_keyboard.py
@@ -1,10 +1,16 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # Buttons for main view commands
-start_help_button = KeyboardButton("start")
-watched_button = KeyboardButton("watched")
-active_button = KeyboardButton("active")
-random_button = KeyboardButton("random")
+start_help_button = KeyboardButton(text="start")
+watched_button = KeyboardButton(text="watched")
+active_button = KeyboardButton(text="active")
+random_button = KeyboardButton(text="random")
 
-kb_view = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_view.row(start_help_button, random_button).row(watched_button, active_button)
+
+kb_view = ReplyKeyboardMarkup(
+    keyboard=[
+        [start_help_button, random_button],
+        [watched_button, active_button],
+    ],
+    resize_keyboard=True
+)
