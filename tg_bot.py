00446ff6fd4a8086ed0e6116b429f2b93852ba74
NikBel523 <belyakov.nkt@gmail.com>
2024-07-05 16:59:08 +0300

    refactor: change handlers to to comply with aiogram 3

--- tg_bot.py
+++ tg_bot.py
@@ -1,45 +1,13 @@
 import logging
 import sys
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from aiogram import Bot, Dispatcher, Router
 from aiohttp import web
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 import config
 from create_bot import bot, router
 from data_base import sqlite_db
-# from handlers import manage_hand, view_hand
-
-
-# Test handlers
-@router.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-
-
-@router.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+from handlers import manage_hand, view_hand
 
 
 async def on_startup(bot: Bot):
@@ -52,11 +20,7 @@ async def on_shutdown(bot: Bot):
     await bot.delete_webhook()
 
 
-# manage_hand.register_manage_handlers(dp=dp)
-# view_hand.register_view_handlers(dp=dp)
-
-
-def main(bot: Bot) -> None:
+def main(bot: Bot, router: Router) -> None:
 
     dp = Dispatcher()
     dp.include_router(router)
@@ -81,4 +45,4 @@ def main(bot: Bot) -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    main(bot)
+    main(bot, router)
