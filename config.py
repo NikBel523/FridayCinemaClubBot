2b3710613220e769d303e19cad631e1f7597dd24
NikBel523 <belyakov.nkt@gmail.com>
2025-03-01 17:31:26 +0300

    refactor: add load_dotenv to config and env_example

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # webhook configurations
 WEB_SERVER_HOST = "127.0.0.1"
-WEB_SERVER_PORT = 8000
-WEBHOOK_PATH = "/webhook"
-BASE_WEBHOOK_URL = "https://cinemabot.strangled.net"
-
+WEB_SERVER_PORT = os.environ.get("WEB_SERVER_PORT")
+WEBHOOK_PATH = os.environ.get("WEBHOOK_PATH")
+BASE_WEBHOOK_URL = os.environ.get("HOST_NAME")
+CERT_PATH = os.environ.get("CERT_PATH")
+CERT_KEY = os.environ.get("CERT_KEY")
 
 TG_TOKEN_API = os.environ.get("TG_TOKEN_API")
